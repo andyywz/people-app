654f89c64aedf1afcb686eceb261362a366b265b
Andy Yong <ayong@mdsol.com>
2014-07-18 12:23:16 -0400

    Adds the /people route that sends data from the people db

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var cors = require('cors');
 
 var app = express();
 app.use(cors());
+
+mongoose.connect('mongodb://localhost/mydb')
+
+var personSchema = {
+  firstName:String,
+  lastName:String,
+  email:String
+};
+
+var Person = mongoose.model('Person', personSchema, 'people');
+
+app.get('/people', function (req, res) {
+  Person.find(function (err, doc) {
+    res.send(doc);
+  });
+});
 
 app.listen(3000);
